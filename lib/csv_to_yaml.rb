fad88873db7e7b50799ddeba9662cc67f4a50f37
Kazuya INOUE <bauer24tbp@gmail.com>
2024-08-03 09:36:33 +0900

    first commit

---
+++ lib/csv_to_yaml.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "csv_to_yaml/version"
+
+module CsvToYaml
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/csv_to_yaml/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CsvToYaml
+  VERSION = "0.1.0"
+end
