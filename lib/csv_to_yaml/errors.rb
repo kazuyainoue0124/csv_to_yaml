54c1dafcce776bdfb1bbf7e21bff8a8945b91699
Kazuya INOUE <bauer24tbp@gmail.com>
2024-08-12 10:33:13 +0900

    Add Error class

--- lib/csv_to_yaml/errors.rb
+++ lib/csv_to_yaml/errors.rb
@@ -3,7 +3,12 @@
 module CsvToYaml
   # Base error class for CsvToYaml
   class Error < StandardError; end
-
   # Error raised when conversion fails
   class ConversionError < Error; end
+  # Error raised when the extension of the input file is not .csv
+  class InvalidExtensionError < Error; end
+  # Error raised when the input file is empty or not readable
+  class EmptyFileError < Error; end
+  # Error raised when the input file is not a valid CSV
+  class InvalidCsvFormatError < Error; end
 end
